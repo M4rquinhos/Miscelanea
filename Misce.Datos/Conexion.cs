edf1eec966c921545fb841dd849a4f058e86f55f
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2023-12-24 16:27:00 -0600

    Conexion con la base de datos se tiene que corregir

---
+++ Misce.Datos/Conexion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misce.Datos
+{
+    public class Conexion
+    {
+        private static Conexion con = null;
+
+        //cadena de conexion
+
+        private Conexion()
+        {
+
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection cadena = new SqlConnection();
+
+            try
+            {
+                cadena.ConnectionString = "Data Source=DESKTOP-TBL2D3P\\SQLEXPRESS;Initial Catalog=MISCELANEA;Integrated Security=True";
+            }
+            catch (Exception ex)
+            {
+
+                cadena = null;
+                throw ex;
+            }
+            return cadena;
+        }
+
+
+        public static Conexion GetInstancia()
+        {
+            if (con == null)
+            {
+                con = new Conexion();
+            }
+            return con;
+        }
+    }
+}
