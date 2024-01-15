accbdac58cf4c49361f633e3b4858b4e19fd5d51
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2024-01-14 22:38:44 -0600

    casi la riego

--- Misce.Datos/BD Scripts/Inserts.sql
+++ Misce.Datos/BD Scripts/Inserts.sql
@@ -1,26 +1,26 @@
-INSERT INTO Tipo_Usuario (Nombre, Descripcion) VALUES
-('ADMIN', 'Administrador de Sistema'),
-('CAJERO', 'Encargado de realizar todo tipo de ventas.'),
-('ALMACENISTA', 'Encargado de Almacen')
-;
+--INSERT INTO Tipo_Usuario (Nombre, Descripcion) VALUES
+--('ADMIN', 'Administrador de Sistema'),
+--('CAJERO', 'Encargado de realizar todo tipo de ventas.'),
+--('ALMACENISTA', 'Encargado de Almacen')
+--;
 
-INSERT INTO Categoria (Nombre, Descripcion) VALUES
-('LIMPIEZA', 'Productos de limpieza en general'),
-('REFRESCOS', 'Refrescos y bebidas mineralizadas en general'),
-('LACTEOS', 'Productos lacteos y derivados');
+--INSERT INTO Categoria (Nombre, Descripcion) VALUES
+--('LIMPIEZA', 'Productos de limpieza en general'),
+--('REFRESCOS', 'Refrescos y bebidas mineralizadas en general'),
+--('LACTEOS', 'Productos lacteos y derivados');
 
-INSERT INTO Estado (Nombre, Nombre_Corto) VALUES
-('DURANGO', 'DGO'),
-('COAHUILA', 'COAH');
+--INSERT INTO Estado (Nombre, Nombre_Corto) VALUES
+--('DURANGO', 'DGO'),
+--('COAHUILA', 'COAH');
 
-INSERT INTO Ciudad (Nombre, Id_Estado) VALUES
-('GOMEZ PALACIO', 1),
-('TORREON', 2);
+--INSERT INTO Ciudad (Nombre, Id_Estado) VALUES
+--('GOMEZ PALACIO', 1),
+--('TORREON', 2);
 
-INSERT INTO Colonia (Nombre, Codigo_Postal, Id_Ciudad) VALUES
-('FIDEL VELAZQUEZ', '35025', 1),
-('COL. CENTRO', '27000', 2);
+--INSERT INTO Colonia (Nombre, Codigo_Postal, Id_Ciudad) VALUES
+--('FIDEL VELAZQUEZ', '35025', 1),
+--('COL. CENTRO', '27000', 2);
 
-INSERT INTO Sucursal (Nombre, Direccion, Id_Colonia, Id_Ciudad) VALUES
-('SUCURSAL MATRIZ', 'Calle 18 andador 23 #317', 1, 1),
-('SUCURSAL FALCON CENTRO', '', 2, 2);
+--INSERT INTO Sucursal (Nombre, Direccion, Id_Colonia, Id_Ciudad) VALUES
+--('SUCURSAL MATRIZ', 'Calle 18 andador 23 #317', 1, 1),
+--('SUCURSAL FALCON CENTRO', '', 2, 2);
--- Misce.Presentacion/Frm_Categorias.Designer.cs
+++ Misce.Presentacion/Frm_Categorias.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            Telerik.WinControls.UI.TableViewDefinition tableViewDefinition2 = new Telerik.WinControls.UI.TableViewDefinition();
+            this.tabPrincipal = new System.Windows.Forms.TabControl();
+            this.tabPFiltros = new System.Windows.Forms.TabPage();
+            this.radGridView1 = new Telerik.WinControls.UI.RadGridView();
+            this.radPanel1 = new Telerik.WinControls.UI.RadPanel();
+            this.tabPInformacion = new System.Windows.Forms.TabPage();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tabPrincipal.SuspendLayout();
+            this.tabPFiltros.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabPrincipal
+            // 
+            this.tabPrincipal.Controls.Add(this.tabPFiltros);
+            this.tabPrincipal.Controls.Add(this.tabPInformacion);
+            this.tabPrincipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPrincipal.Location = new System.Drawing.Point(1, 75);
+            this.tabPrincipal.Name = "tabPrincipal";
+            this.tabPrincipal.SelectedIndex = 0;
+            this.tabPrincipal.Size = new System.Drawing.Size(1159, 538);
+            this.tabPrincipal.TabIndex = 0;
+            // 
+            // tabPFiltros
+            // 
+            this.tabPFiltros.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(218)))), ((int)(((byte)(213)))));
+            this.tabPFiltros.Controls.Add(this.radGridView1);
+            this.tabPFiltros.Controls.Add(this.radPanel1);
+            this.tabPFiltros.Location = new System.Drawing.Point(4, 25);
+            this.tabPFiltros.Name = "tabPFiltros";
+            this.tabPFiltros.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPFiltros.Size = new System.Drawing.Size(1151, 509);
+            this.tabPFiltros.TabIndex = 0;
+            this.tabPFiltros.Text = "Busqueda";
+            // 
+            // radGridView1
+            // 
+            this.radGridView1.Location = new System.Drawing.Point(6, 183);
+            // 
+            // 
+            // 
+            this.radGridView1.MasterTemplate.ViewDefinition = tableViewDefinition2;
+            this.radGridView1.Name = "radGridView1";
+            this.radGridView1.Size = new System.Drawing.Size(1139, 320);
+            this.radGridView1.TabIndex = 1;
+            // 
+            // radPanel1
+            // 
+            this.radPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(215)))), ((int)(((byte)(90)))));
+            this.radPanel1.Location = new System.Drawing.Point(6, 29);
+            this.radPanel1.Name = "radPanel1";
+            this.radPanel1.Size = new System.Drawing.Size(1139, 134);
+            this.radPanel1.TabIndex = 0;
+            this.radPanel1.ThemeName = "ControlDefault";
+            // 
+            // tabPInformacion
+            // 
+            this.tabPInformacion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(218)))), ((int)(((byte)(213)))));
+            this.tabPInformacion.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.tabPInformacion.Location = new System.Drawing.Point(4, 25);
+            this.tabPInformacion.Name = "tabPInformacion";
+            this.tabPInformacion.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPInformacion.Size = new System.Drawing.Size(1151, 509);
+            this.tabPInformacion.TabIndex = 1;
+            this.tabPInformacion.Text = "Informacion";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(78)))));
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.toolStrip1.Location = new System.Drawing.Point(2, 2);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1158, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Frm_Categorias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Frm_Categorias";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(105)))), ((int)(((byte)(129)))));
+            this.ClientSize = new System.Drawing.Size(1162, 610);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.tabPrincipal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Frm_Categorias";
+            this.Padding = new System.Windows.Forms.Padding(2);
+            this.Text = "Categorias";
+            this.Load += new System.EventHandler(this.Frm_Categorias_Load);
+            this.tabPrincipal.ResumeLayout(false);
+            this.tabPFiltros.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabPrincipal;
+        private System.Windows.Forms.TabPage tabPFiltros;
+        private System.Windows.Forms.TabPage tabPInformacion;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private Telerik.WinControls.UI.RadPanel radPanel1;
+        private Telerik.WinControls.UI.RadGridView radGridView1;
     }
 }
